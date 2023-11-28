28bb9d58d3f2c8ae0168a906ae71ecba80791954
Zin Hoang <hoang.mantue@gmail.com>
2023-11-28 20:36:45 +1300

    Switch from using pure css to styled component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import ReactDom from 'react-dom/client';
 import App from './App';
-import './index.css';
+import { GlobalStyle } from './styles/GlobalStyle';
+import React from 'react';
 
 const root = ReactDom.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
